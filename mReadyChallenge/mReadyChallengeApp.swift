c5d4625742e10a73559a7ed90eed7fc9329cdd4b
Camelia Braghes <cameliabraghes@gmail.com>
2022-06-17 18:22:04 +0300

    Initial Commit

---
+++ mReadyChallenge/mReadyChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mReadyChallengeApp.swift
+//  mReadyChallenge
+//
+//  Created by Camelia Braghes on 17.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct mReadyChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
